358579655e957adcc19a819384fd0ff6528afcf8
nd0ut <nd0ut.me@gmail.com>
2021-02-26 18:07:38 +0300

    wip

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,3 +1,10 @@
+const NetlifyAPI = require('netlify')
+const client = new NetlifyAPI(process.env.NETLIFY_ACCESS_TOKEN)
+
 exports.handler = function(event, context, callback) {
-  console.log(event,context,callback);
+  console.log('START HANDLER')
+  console.log('START_EVENT', event, 'END_EVENT')
+  console.log('START_CONTEXT', context, 'END_CONTEXT')
+  console.log('START_CALLBACK', callback, 'END_CALLBACK')
+  console.log('END HANDLER')
 }
